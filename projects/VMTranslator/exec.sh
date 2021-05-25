da688c6444e0141046287eb214a94776262f3057
Bereket Tewoldeberhan <btesfamichael@clincase.com>
2021-05-25 17:05:19 +0200

    moved assignment directories to upper levelg

--- projects/VMTranslator/exec.sh
+++ projects/VMTranslator/exec.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-#export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/07/StackArithmetic/SimpleAdd/'
+export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/07/StackArithmetic/SimpleAdd/'
 #export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/07/StackArithmetic/StackTest/'
 #export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/07/MemoryAccess/StaticTest/'
 #export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/07/MemoryAccess/BasicTest/'
 #export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/08/ProgramFlow/BasicLoop/'
-export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/08/ProgramFlow/FibonacciSeries/'
+#export JAVA_PROGRAM_ARGS='/Users/btesfamichael/Documents/nand_to_tetris/projects/08/ProgramFlow/FibonacciSeries/'
 mvn exec:java -Dexec.mainClass="VMTranslator" -Dexec.args="$JAVA_PROGRAM_ARGS"
 ls -l $JAVA_PROGRAM_ARGS
